0047f187607939c1357cf19e1d38183ee48842ef
kwpak8 <37367883+kwpak8@users.noreply.github.com>
2023-01-01 23:41:03 +0900

    폰트 사이즈 수정. 갤러리 구현

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var num = 0;
+var naviPage = 1;
 
 function Setting(){
 	var account1 = document.getElementById('account1');
@@ -26,7 +27,56 @@ function AccountShow(num){
 
 function CloseAll(){
 	Setting();
-	
+
 	account1.style.display = "none";
 	account2.style.display = "none";	
+}
+
+function NaviNext(){
+	var photoList = document.getElementById('photoList');
+	var galleryBtnLeft = document.getElementById('galleryBtnLeft');
+	var galleryBtnRight = document.getElementById('galleryBtnRight');
+	var width = 974;
+	if(naviPage < 4){
+		naviPage = naviPage + 1;
+		var margin = (naviPage-1) * (-974);
+		photoList.style.marginLeft = margin;
+		
+	}else{
+		
+	}
+
+	NaviBtnSetting();
+}
+
+function NaviPrev(){
+	var photoList = document.getElementById('photoList');
+	var width = 974;
+	if(naviPage > 1){
+		naviPage = naviPage - 1;
+		var margin = (naviPage-1) * (-974);
+		photoList.style.marginLeft = margin;
+	}else{
+
+	}
+
+	NaviBtnSetting();
+}
+
+function NaviBtnSetting(){
+	if(naviPage > 1){
+		galleryBtnLeft.style.opacity = '100%';
+	}
+
+	if(naviPage < 4){
+		galleryBtnRight.style.opacity = '100%';
+	}
+
+	if(naviPage == 1){
+		galleryBtnLeft.style.opacity = '30%';
+	}
+
+	if(naviPage == 4){
+		galleryBtnRight.style.opacity = '30%';
+	}
 }
